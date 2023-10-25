eeeddaaaa1f4623a5d9ffe6664db0f0a4aba1c65
anthony <tony20402014@gmail.com>
2023-10-25 12:47:21 -0600

    agregando el material 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ android {
     namespace = "com.example.feedspot"
     compileSdk = 33
 
+
     defaultConfig {
         applicationId = "com.example.feedspot"
         minSdk = 29
@@ -32,11 +33,12 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
 }
